2ea87cd03864896d8139d470664122c9660a7057
unknown <rockydasgfx@gmail.com>
2022-05-02 12:01:21 +0600

    add search

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("start assignment");
+document.getElementById("search-btn").addEventListener("click", () => {
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  const inLowerCase = searchText.toLowerCase();
+
+  // clean field text
+  searchField.value = "";
+
+  //data load
+  function lodeData() {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${inLowerCase}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }
+  lodeData();
+});
